ab66a293128c2a781c4472b4ac0cb4171ead3a95
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-04-03 14:51:33 -0500

    Added a Navbar.Toggle to be displayed on smaller screens to display nav links

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -33,6 +33,7 @@ export default function Header() {
         <Link to="/sign-in">
           <Button gradientDuoTone="purpleToBlue">Sign In</Button>
         </Link>
+        <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
         <Navbar.Link>
