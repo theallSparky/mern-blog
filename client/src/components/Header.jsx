f82244cc85146f7406c0114fff8e5249e270a7e4
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-04-03 12:29:45 -0500

    Stylized the header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,18 @@
 import { Navbar } from "flowbite-react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-  return <Navbar>Header</Navbar>;
+  return (
+    <Navbar className="border-b-2">
+      <Link
+        to="/"
+        className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+      >
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-600 via-purple-400 to-orange-400 rounded-lg text-white">
+          Harry&apos;s{" "}
+        </span>
+        Blog
+      </Link>
+    </Navbar>
+  );
 }
