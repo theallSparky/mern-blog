29fc749bd1c48084f103aa66a31d67dec87f1ff5
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-04-03 14:47:40 -0500

    Added buttons to be visible on md screens and above to navigate to home, /projects and /about

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -26,13 +26,24 @@ export default function Header() {
       <Button className="w-12 h-10 lg:hidden" color="gray" pill>
         <AiOutlineSearch />
       </Button>
-      <div className="flex">
+      <div className="flex gap-2 md:order-2">
         <Button className="w-12 h-10 hidden sm:inline" color="gray" pill>
           <FaMoon />
         </Button>
         <Link to="/sign-in">
           <Button gradientDuoTone="purpleToBlue">Sign In</Button>
         </Link>
+        <Navbar.Collapse>
+          <Navbar.Link>
+            <Link to="/">Home</Link>
+          </Navbar.Link>
+          <Navbar.Link>
+            <Link to="/about">About</Link>
+          </Navbar.Link>
+          <Navbar.Link>
+            <Link to="/projects">Projects</Link>
+          </Navbar.Link>
+        </Navbar.Collapse>
       </div>
     </Navbar>
   );
