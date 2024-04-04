df4ee8210621c06f13b4a038441425a0e345a157
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-04-04 14:02:27 -0500

    Downloaded and configured dotenv to connect to mongodb database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODBURI)
+  .then(() => {
+    console.log("Mongodb connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
