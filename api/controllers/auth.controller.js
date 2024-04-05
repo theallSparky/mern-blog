ca1f431f66b1cd1c9e1f131499f5ce16ae492d88
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-04-05 09:53:11 -0500

    Made middleware error handler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -12,7 +13,7 @@ export const signup = async (req, res, next) => {
     email === "" ||
     password === ""
   ) {
-    return res.status(400).json({ message: "All fields are required" });
+    next(errorHandler(400, "All fields are required."));
   }
 
   const hashedPassword = bcryptjs.hashSync(password, 10);
