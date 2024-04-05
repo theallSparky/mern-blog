79a99804c7f51d25c99e0d3411dfcef21039bc18
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-04-05 10:15:49 -0500

    Began working on signup page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,3 +1,24 @@
+import { Link } from "react-router-dom";
+
 export default function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <div className="min-h-screen mt-20">
+      <div className="">
+        {/* left */}
+        <div className="">
+          <Link
+            to="/"
+            className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+          >
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-600 via-purple-400 to-orange-400 rounded-lg text-white">
+              Harry&apos;s{" "}
+            </span>
+            Blog
+          </Link>
+        </div>
+        {/* right */}
+        <div className=""></div>
+      </div>
+    </div>
+  );
 }
