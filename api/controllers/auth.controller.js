903008847fc68a4e70794c44a8ae754def7f71b0
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-04-05 09:40:28 -0500

    Setup the middleware to return error message

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
-import User from "../models/user.models.js";
+import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
   if (
@@ -12,21 +12,21 @@ export const signup = async (req, res) => {
     email === "" ||
     password === ""
   ) {
-    return res.status(400).json({ message: "All fields are required." });
+    return res.status(400).json({ message: "All fields are required" });
   }
 
-  const hashedPassword = bcryptjs.hashSync(password, 12);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
-  const newUser = new User({ username, email, password: hashedPassword });
-
-  await newUser.save();
-  res.json("Signup successful");
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
 
   try {
     await newUser.save();
     res.json("Signup successful");
   } catch (error) {
-    res.status(500).json({ message: error.message });
-    console.log(error.message);
+    next(error);
   }
 };
