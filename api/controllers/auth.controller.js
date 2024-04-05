55d47bb888b230c6bef46183941089acc1ebfca3
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-04-05 08:52:57 -0500

    Finished signup api work

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,3 +1,32 @@
+import User from "../models/user.models.js";
+import bcryptjs from "bcryptjs";
+
 export const signup = async (req, res) => {
-  console.log(req.body);
+  const { username, email, password } = req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return res.status(400).json({ message: "All fields are required." });
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 12);
+
+  const newUser = new User({ username, email, password: hashedPassword });
+
+  await newUser.save();
+  res.json("Signup successful");
+
+  try {
+    await newUser.save();
+    res.json("Signup successful");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log(error.message);
+  }
 };
