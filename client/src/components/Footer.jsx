abda36dd79ee1a3d4550351bb51bca27449f2f4a
Harrison Sparks <harrisonsparks.1999@gmail.com>
2024-04-07 20:52:10 -0500

    Successfully made 1st footer linkgroup linking to /about page and personal portfolio page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,47 @@
-import React from "react";
+import { Footer } from "flowbite-react";
+import { Link } from "react-router-dom";
 
-export default function Footer() {
-  return <div>Footer</div>;
+export default function FooterComponent() {
+  return (
+    <Footer container className="border border-t-8 border-teal-500">
+      <div className="">
+        <div className="">
+          <div className="">
+            <Link
+              to="/"
+              className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
+            >
+              <span className="px-2 py-1 bg-gradient-to-r from-indigo-600 via-purple-400 to-orange-400 rounded-lg text-white">
+                Harry&apos;s{" "}
+              </span>
+              Blog
+            </Link>
+          </div>
+          <div className="grid grid-cols-2 gap-3 sm:mt-4 sm:grid-cols-3 sm:gap-6">
+            <div className="">
+              <Footer.Title title="About" />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="https://harrison-sparks-portfolio-v1.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Portfolio Website
+                </Footer.Link>
+              </Footer.LinkGroup>
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href="/about"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Harry&apos;s Blog
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
 }
